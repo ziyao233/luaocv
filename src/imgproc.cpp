b6acfa091fb3338a9214f6887b1d713cef6c251a
Yao Zi <ziyao@disroot.org>
2024-02-05 23:02:28 +0800

    Bind more arguments for locv.line()

--- src/imgproc.cpp
+++ src/imgproc.cpp
@@ -10,14 +10,29 @@
 
 #include"core.hpp"
 
+static int
+locv_imgproc_line_type_name_to_id(lua_State *l, int idx)
+{
+	const char *names[] = {
+		"filled", "line4", "line8", "antialiased",
+		NULL,
+	};
+	int ids[] = {
+		cv::FILLED, cv::LINE_4, cv::LINE_8, cv::LINE_AA,
+	};
+	return ids[luaL_checkoption(l, idx, "line8", names)];
+}
+
 int
 locv_imgproc_line(lua_State *l)
 {
-	cv::Mat *mat = locv_core_mat_in_native(l, 1);
-	cv::Point p1 = locv_core_point_to_native(l, 2);
-	cv::Point p2 = locv_core_point_to_native(l, 3);
-	cv::Scalar s = locv_core_scalar_to_native(l, 4);
-	cv::line(*mat, p1, p2, s);
+	cv::Mat *mat	= locv_core_mat_in_native(l, 1);
+	cv::Point p1	= locv_core_point_to_native(l, 2);
+	cv::Point p2	= locv_core_point_to_native(l, 3);
+	cv::Scalar s	= locv_core_scalar_to_native(l, 4);
+	int thickness	= luaL_optinteger(l, 5, 1);
+	int type	= locv_imgproc_line_type_name_to_id(l, 6);
+	cv::line(*mat, p1, p2, s, thickness, type);
 	lua_settop(l, 1);
 	return 1;
 }
